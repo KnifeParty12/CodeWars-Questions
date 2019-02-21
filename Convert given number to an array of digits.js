68b7772286e8aeda1980051c7a6ff71f316039f4
KnifeParty12 <flashfunk01@gmail.com>
2019-02-21 21:17:26 +0530

    Gotta Learn REGEX

---
+++ Convert given number to an array of digits.js
@@ -0,0 +1,4 @@
+
+var n = 32143546563;
+var yo = n.toString(10).split('').map(Number);
+console.log(yo);
---
+++ Descending Order.js
@@ -0,0 +1,10 @@
+function x(yo){
+    return parseInt(String(yo).split('').sort().reverse().join(''));
+
+}
+
+var n = 23143524;
+
+x(n);
+
+
